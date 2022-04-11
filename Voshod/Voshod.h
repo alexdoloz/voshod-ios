a45d90000745e7ca4dd2a9392f0f04e11a22a967
Alexander Doloz <ADoloz@bellintegrator.com>
2022-04-11 04:47:52 +0300

    Initial Commit

---
+++ Voshod/Voshod.h
@@ -0,0 +1,18 @@
+//
+//  Voshod.h
+//  Voshod
+//
+//  Created by Alexander Doloz on 11.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Voshod.
+FOUNDATION_EXPORT double VoshodVersionNumber;
+
+//! Project version string for Voshod.
+FOUNDATION_EXPORT const unsigned char VoshodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Voshod/PublicHeader.h>
+
+
